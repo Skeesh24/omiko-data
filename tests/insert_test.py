fbd48c715580d4981e91cc72ca15293860eec185
skeesh24 <d.pacooloff@gmail.com>
2023-07-23 23:59:42 +1000

    small fixes in insert test

--- tests/insert_test.py
+++ tests/insert_test.py
@@ -6,8 +6,9 @@ from settings import Settings
 def test_select_query():
     response = post(
         Settings.url + "/insert/user",
-        json={"email": "bratok@gmail.com", "password": "AESRGEFfnpsiyhPYBEFncwpnsSUkm"},
+        json={
+            "email": "bratok2@gmail.com",
+            "password": "AESRGEFfnpsiyhPYBEFncwpnsSUkm",
+        },
     )
     assert response.status_code == codes.created
-    assert response.json() is not None
-    assert len(response.json()) >= 1
