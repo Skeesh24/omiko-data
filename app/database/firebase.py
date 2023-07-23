412f8190ece911100b0d9d9a05dde5f3e853f154
skeesh24 <d.pacooloff@gmail.com>
2023-07-23 16:47:24 +1000

    main script added

--- app/database/firebase.py
+++ app/database/firebase.py
@@ -1,9 +1,5 @@
 from fireo import connection as firebase_connection
-from models import User
 
 
-firebase_connection(from_file="certificate.json")
-
-
-user: User = User.collection.limit(1).get()
-print(user.to_dict())
+def initialize():
+    firebase_connection(from_file="certificate.json")
---
+++ main.py
@@ -0,0 +1,8 @@
+from app.database.firebase import initialize as db_initialize
+from app.routers.root_route import app
+from uvicorn import run
+
+
+if __name__ == "__main__":
+    db_initialize()
+    run(app, host="localhost", port=8000)
