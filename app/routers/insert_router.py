0e3465509b812c59193bda79783c7f858e8f0980
skeesh24 <d.pacooloff@gmail.com>
2023-07-24 21:57:28 +1000

    request and response  validation added

--- app/routers/insert_router.py
+++ app/routers/insert_router.py
@@ -2,14 +2,22 @@ from fastapi import APIRouter, Body
 from fastapi.params import Depends
 from requests.status_codes import codes
 from fireo.database import Database
+from fastapi.exceptions import HTTPException
 
 from app.database.firebase import get_db
+from app.models.fastapi_models import UserRequestModel, UserResponseModel
 
 
 insert_router = APIRouter(prefix="/insert", tags=["insert", "post"])
 
 
-@insert_router.post("/{tablename}", status_code=codes.created)
+@insert_router.post(
+    "/{tablename}", status_code=codes.created, response_model=UserResponseModel
+)
 async def insert(tablename: str, data: dict = Body(), db: Database = Depends(get_db)):
-    ins = db.conn.collection(tablename).add(data)
-    print(ins)
+    try:
+        UserRequestModel(**data)
+    except Exception as e:
+        raise HTTPException(status_code=codes.bad_request, detail=str(e))
+    db.conn.collection(tablename).add(data)
+    return UserResponseModel(**data)
