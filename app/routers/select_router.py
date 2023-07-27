c5ea48e18ea5f02787a2e0b7c0a9b30f9e13cd7c
skeesh24 <d.pacooloff@gmail.com>
2023-07-27 19:29:36 +1000

    all params are active in the select router now

--- app/routers/select_router.py
+++ app/routers/select_router.py
@@ -2,6 +2,7 @@ from typing import List
 from fastapi import APIRouter
 from app.database.entities import Product, User
 from app.models.repositories.repository import FirebaseRepository
+from app.models.repositories.repository_interface import IRepository
 
 from app.models.validation import (
     FilterRequestModel,
@@ -21,25 +22,26 @@ select_router = APIRouter(prefix="/select", tags=["select"])
 
 
 @select_router.get("/user", response_model=List[UserResponseModel])
-async def select(
-    limit: int = 5,
-    offset: int = 0,
-    where: FilterRequestModel = None,
-    db: FirebaseRepository[User] = Depends(get_user_repository),
-) -> list[UserResponseModel]:
-    all = db.get_all()
-
-    return [UserResponseModel(**x.to_dict()) for x in all]
-
-
-@select_router.get("/product", response_model=ProductResponseModel)
-async def select_product(
+async def select_users(
     limit: int = 5,
     offset: int = 0,
     document_id: str = "",
     where: FilterRequestModel = None,
-    db: FirebaseRepository[Product] = Depends(get_product_repository),
-) -> ProductResponseModel:
-    element = db.get(document_id=document_id)
+    db: IRepository = Depends(get_user_repository),
+) -> list[UserResponseModel]:
+    users = db.get(limit=limit, offset=offset, document_id=document_id, where=where)
 
-    return element
+    return [UserResponseModel(**x.to_dict()) for x in users]
+
+
+@select_router.get("/product", response_model=ProductResponseModel)
+async def select_products(
+    limit: int = 5,
+    offset: int = 0,
+    document_id: str = "",
+    where: FilterRequestModel = None,
+    db: IRepository = Depends(get_product_repository),
+) -> ProductResponseModel:
+    products = db.get(limit=limit, offset=offset, document_id=document_id, where=where)
+
+    return products
