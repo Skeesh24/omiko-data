963a9907ea8f715e847d69e0c9fb9a099a572a90
skeesh24 <d.pacooloff@gmail.com>
2023-07-30 02:51:17 +1000

    delete router''s fixes

--- app/routers/delete_router.py
+++ app/routers/delete_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
 from requests import codes
-from app.classes.dependencies import get_user_repository
+from app.classes.dependencies import get_product_repository, get_user_repository
 from app.models.repositories.repository_interface import IRepository
 
 from app.models.validation import FilterRequestModel
@@ -10,10 +10,9 @@ from app.models.validation import FilterRequestModel
 delete_router = APIRouter(prefix="/delete", tags=["delete"])
 
 
-@delete_router.delete("/user", status_code=codes.NO_CONTENT)
+@delete_router.delete("/product", status_code=codes.NO_CONTENT)
 async def delete_user(
     document_id: str = "",
-    where: FilterRequestModel = None,
-    db: IRepository = Depends(get_user_repository),
+    db: IRepository = Depends(get_product_repository),
 ) -> None:
-    db.remove(document_id=document_id, where=where)
+    db.remove(document_id=document_id)
