860d91dbe1460ebd3f8955aae6a0bc812b20f245
skeesh24 <d.pacooloff@gmail.com>
2023-07-30 02:51:39 +1000

    async tests for deleting

--- tests/delete_test.py
+++ tests/delete_test.py
@@ -1,5 +1,5 @@
-from requests import delete
-from requests.status_codes import codes
+from httpx import AsyncClient
+from requests import codes
 from settings import Settings
 import pytest
 
@@ -7,37 +7,31 @@ import pytest
 ROUTE = Settings.URL + Settings.DELETE
 
 
+@pytest.mark.anyio
 @pytest.mark.parametrize(
-    "prefix, params, data",
+    "prefix, params",
     [
-        ("/user", None, None),
+        # ("/user", None),
         (
             "/product",
-            None,
-            {"field_path": "name", "op_string": "==", "value": "test"},
+            {"document_id": "pa9xFlcIXcyDvCA5onWE"},
         ),
-        (
-            "/order",
-            None,
-            {
-                "user": "bratok2@gmail.com",
-                "products": ["test"],
-                "price": 24,
-            },
-        ),
-        (
-            "/product_category",
-            {"document_id": "U0Kg4CtDyNRTnLpfa08V"},
-            {"name": "test", "product_count": 24},
-        ),
-        (
-            "/office",
-            {},
-            {"city": "test", "address": "test", "phone": "test", "email": "test"},
-        ),
-        ("/cabinet", None, None),
+        # (
+        #     "/order",
+        #     None,
+        # ),
+        # (
+        #     "/product_category",
+        #     {"document_id": "U0Kg4CtDyNRTnLpfa08V"},
+        # ),
+        # (
+        #     "/office",
+        #     {},
+        # ),
+        # ("/cabinet", None),
     ],
 )
-def test_delete(prefix, params, data):
-    response = delete(ROUTE + prefix, params=params, json=data)
+async def test_delete(prefix, params):
+    async with AsyncClient(base_url=ROUTE) as ac:
+        response = await ac.delete(prefix, params=params)
     assert response.status_code == codes.NO_CONTENT
