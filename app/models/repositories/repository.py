2e1339673629406e099bd05dc2b0f6175aa251af
skeesh24 <d.pacooloff@gmail.com>
2023-07-29 16:45:31 +1000

    fixes in docs and selection logic

--- app/models/repositories/repository.py
+++ app/models/repositories/repository.py
@@ -1,5 +1,6 @@
 from typing import Generic, List, TypeVar
 from fireo.models import Model
+from fireo.database import Database
 from app.database.firebase import get_db
 from app.models.repositories.fields import fields
 from app.models.repositories.repository_interface import IRepository
@@ -17,43 +18,43 @@ class FirebaseRepository(Generic[_T], IRepository):
     ## This repository provides access to the firebase
 
     1. get - returns a document with the given id
-    2. get_all - returns all documents
-    3. add - adds a document
-    4. update - updates a document by the given id
-    5. remove - removes a document by the given id
+    2. add - adds a document
+    3. update - updates a document by the given id
+    4. remove - removes a document by the given id
     """
 
     def __init__(self) -> None:
-        pass
+        self.db = get_db()
 
     def connect(self):
-        self.db = get_db()
         type = self.__orig_class__.__args__[0]
-
         return self.db.conn.collection(type.collection_name)
 
     def get(
         self, limit: int, offset: int, document_id: str, where: FilterRequestModel
     ) -> List[_T]:
         """
-        ## Get a document by id
+        ## Gets a document by id
 
         1. param limit - the number of documents to retrieve
         2. param offset - the offset to get the document
         3. param document_id: the id of the document from the generic collection
+        4. param where - the filter to get the document
 
         ### returns a list of documents
         """
-        element = self.connect()
-        if document_id == "":
-            element = element.limit(limit).offset(offset)
-            if where:
-                element = element.where(**where.dict(exclude_defaults=True))
-        else:
-            element = element.document(document_id=document_id)
+        query = self.connect()
+        try:
+            if document_id == "":
+                if where:
+                    query = query.where(**where.dict(exclude_defaults=True))
+                query = query.limit(limit).offset(offset)
+            else:
+                query = query.document(document_id=document_id)
+        except:
+            raise HTTPException(status_code=400)
 
-        # element = self.connect().where(fields.ID, signs.EQ, document_id).get()
-        return element.get()
+        return query.get()
 
     def add(self, element: _T) -> None:
         """
@@ -63,11 +64,12 @@ class FirebaseRepository(Generic[_T], IRepository):
 
         ### returns None or raises exception
         """
+        query = self.connect()
 
         try:
-            self.connect().add(element.dict(exclude_defaults=True))
-        except Exception as e:
-            raise HTTPException(status_code=400, detail=str(e))
+            query.add(element.dict(exclude_defaults=True))
+        except Exception:
+            raise HTTPException(status_code=400)
 
     def update(self, document_id: str, element: _T) -> None:  # TODO debug
         """
@@ -83,21 +85,28 @@ class FirebaseRepository(Generic[_T], IRepository):
             self.remove(document_id)
             d: dict = element.dict(exclude_defaults=True)
             d[fields.ID] = document_id
-            self.connect().add(dict)
+            self.connect().add(d)
 
         except Exception:
             raise HTTPException(status_code=400)
 
-    def remove(self, document_id: str) -> None:  # TODO debug
+    def remove(self, document_id: str, where: FilterRequestModel) -> None:  # TODO debug
         """
         ## Removes document by the document's id
 
         1. param document_id: the id of the document from the generic collection
+        2. param where: the filter to remove the documents
 
         ### returns None or raises exception
         """
+        query = self.connect()
 
         try:
-            self.connect().document(document_id).delete()
+            if where:
+                query.where(**where.dict(exclude_defaults=True))
+            if document_id != "":
+                query = query.document(document_id)
         except Exception:
             raise HTTPException(status_code=400)
+
+        query.delete()
