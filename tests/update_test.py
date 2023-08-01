8e7e5d24390ca6a5b43c70f096fa994dde854e9e
skeesh24 <d.pacooloff@gmail.com>
2023-08-01 23:50:38 +1000

    update router tests

--- tests/update_test.py
+++ tests/update_test.py
@@ -8,10 +8,48 @@ from settings import Settings
 @pytest.mark.parametrize(
     "prefix, params, json",
     [
-        ("/product", {}, {}),
-        ("/order", {}, {}),
-        ("product_category", {}, {}),
-        ("order_category", {}, {}),
+        (
+            "/user",
+            {"document_id": "1FbLOpGcjFdYWYil9Oow"},
+            {"email": "bratok2@gmail.com", "password": "apple"},
+        ),
+        (
+            "/product",
+            {"document_id": "DuQ3dJMuBSLaCZemlgg9"},
+            {
+                "category": "C",
+                "description": "D",
+                "name": "N",
+                "price": 2400,
+                "short_description": "SD",
+            },
+        ),
+        (
+            "/order",
+            {"document_id": "6skiPrgAKrDDtcmBfnuT"},
+            {"price": 240, "products": ["apple"], "user": "bratok2@gmail.com"},
+        ),
+        (
+            "/product_category",
+            {"document_id": "8BCvXNR1ljzR95O35DB5"},
+            {"name": "apple", "product_count": 24},
+        ),
+        (
+            "/office",
+            {"document_id": "OVwFEbzJNfoHRpCi7Cg3"},
+            {"address": "apple", "city": "apple", "email": "apple", "phone": "apple"},
+        ),
+        (
+            "/cabinet",
+            {"document_id": "18hB8529EU0GcypXmQR4"},
+            {
+                "cart": ["apple"],
+                "city": "apple",
+                "favourites": ["apple"],
+                "orders": ["apple"],
+                "phone": "apple",
+            },
+        ),
     ],
 )
 async def test_update_all_routes(prefix, params, json):
