3e4655be27f5b488b74ae9f98e8568de9af0c29c
skeesh24 <d.pacooloff@gmail.com>
2023-08-01 23:36:45 +1000

    delete simplifing

--- app/routers/delete_router.py
+++ app/routers/delete_router.py
@@ -2,64 +2,19 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from requests import codes
 from app.classes.dependencies import (
-    get_cabinet_repository,
-    get_office_repository,
-    get_order_repository,
-    get_product_category_repository,
-    get_product_repository,
-    get_user_repository,
+    get_uow,
 )
 from app.models.repositories.repository_interface import IRepository
-
-from app.models.validation import FilterRequestModel
 
 
 delete_router = APIRouter(prefix="/delete", tags=["delete"])
 
 
-@delete_router.delete("/user", status_code=codes.NO_CONTENT)
+@delete_router.delete("/{tablename}", status_code=codes.NO_CONTENT)
 async def delete_user(
+    tablename: str,
     document_id: str = "",
-    db: IRepository = Depends(get_user_repository),
+    uow=Depends(get_uow),
 ) -> None:
-    db.remove(document_id=document_id)
-
-
-@delete_router.delete("/product", status_code=codes.NO_CONTENT)
-async def delete_user(
-    document_id: str = "",
-    db: IRepository = Depends(get_product_repository),
-) -> None:
-    db.remove(document_id=document_id)
-
-
-@delete_router.delete("/order", status_code=codes.NO_CONTENT)
-async def delete_user(
-    document_id: str = "",
-    db: IRepository = Depends(get_order_repository),
-) -> None:
-    db.remove(document_id=document_id)
-
-
-@delete_router.delete("/product_category", status_code=codes.NO_CONTENT)
-async def delete_user(
-    document_id: str = "",
-    db: IRepository = Depends(get_product_category_repository),
-) -> None:
-    db.remove(document_id=document_id)
-
-
-@delete_router.delete("/office", status_code=codes.NO_CONTENT)
-async def delete_user(
-    document_id: str = "",
-    db: IRepository = Depends(get_office_repository),
-) -> None:
-    db.remove(document_id=document_id)
-
-
-@delete_router.delete("/cabinet", status_code=codes.NO_CONTENT)
-async def delete_user(
-    document_id: str = "",
-    db: IRepository = Depends(get_cabinet_repository),
-) -> None:
+    db: IRepository = uow.__getattribute__(tablename)
     db.remove(document_id=document_id)
