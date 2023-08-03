3ad6abc22e4f236f07d6e89797f9779614c60f3b
skeesh24 <d.pacooloff@gmail.com>
2023-08-03 23:56:07 +1000

    fix environ issue

--- app/database/firebase.py
+++ app/database/firebase.py
@@ -4,28 +4,30 @@ from os import environ
 
 
 def certificate_modifing():
-    with open(curdir + "/certificate.json", "w") as f:
+    with open("c:/users/skeesh/desktop" + "/certificate.json", "w") as f:
         f.write(
             """
-                {\n
-                    "type": "service_account",\n
-                    "project_id": "omiko-data",\n
-                    "private_key_id": "%s",\n
-                    "private_key": "%s",\n
-                    "client_email": "%s",\n
-                    "client_id": "%s",\n
-                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",\n
-                    "token_uri": "https://oauth2.googleapis.com/token",\n
-                    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",\n
-                    "client_x509_cert_url": "%s",\n
-                    "universe_domain": "googleapis.com",\n
-                }\n
+{
+    "type": "service_account",
+    "project_id": "omiko-data",
+    "private_key_id": "%s",
+    "private_key": "%s",
+    "client_email": "%s",
+    "client_id": "%s",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "%s",
+    "universe_domain": "googleapis.com"
+}
             """
-            % environ.get("PRIVATE_KEY_ID")
-            % environ.get("PRIVATE_KEY")
-            % environ.get("CLIENT_EMAIL")
-            % environ.get("CLIENT_ID")
-            % environ.get("CLIENT_X509_CERT_URL")
+            % (
+                environ.get("PRIVATE_KEY_ID"),
+                environ.get("PRIVATE_KEY"),
+                environ.get("CLIENT_EMAIL"),
+                environ.get("CLIENT_ID"),
+                environ.get("CLIENT_X509_CERT_URL"),
+            )
         )
 
 
