56c0d9279620b7153bb9a90fe13e329ae04331e0
skeesh24 <d.pacooloff@gmail.com>
2023-08-08 17:33:19 +1000

    certificate generating udpate

--- app/database/firebase.py
+++ app/database/firebase.py
@@ -1,27 +1,37 @@
 from fireo import connection as firebase_connection
 from os.path import curdir
-from os import environ
+from os import environ, remove
 
 
-def get_credentials_object():
-    return {
-        "type": "service_account",
-        "project_id": "omiko-data",
-        "private_key_id": environ.get("PRIVATE_KEY_ID"),
-        "private_key": environ.get("PRIVATE_KEY"),
-        "client_email": environ.get("CLIENT_EMAIL"),
-        "client_id": environ.get("CLIENT_ID"),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": environ.get("CLIENT_X509_CERT_URL"),
-        "universe_domain": "googleapis.com",
-    }
+cert = """
+{
+    "type": "service_account",
+    "project_id": "omiko-data",
+    "private_key_id": "%s",
+    "private_key": "%s",
+    "client_email": "%s",
+    "client_id": "%s",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "%s",
+    "universe_domain": "googleapis.com"
+}
+""" % (
+    environ.get("PRIVATE_KEY_ID"),
+    environ.get("PRIVATE_KEY"),
+    environ.get("CLIENT_EMAIL"),
+    environ.get("CLIENT_ID"),
+    environ.get("CLIENT_X509_CERT_URL"),
+)
 
 
 def initialize():
-    creds = get_credentials_object()
-    firebase_connection(credentials=creds)
+    path = "certificate.json"
+    with open(f"{curdir}/{path}", "w") as file:
+        file.write(cert)
+    firebase_connection(from_file=path)
+    remove(path)
 
 
 def get_db():
