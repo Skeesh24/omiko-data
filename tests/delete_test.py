e7c0e41bed50eaf7d202309ff2e6a455eb86226f
skeesh24 <d.pacooloff@gmail.com>
2023-08-09 19:52:56 +1000

    delete tests update

--- tests/delete_test.py
+++ tests/delete_test.py
@@ -1,10 +1,14 @@
-from httpx import Client
-from requests import codes
+from httpx import Client, codes
 from settings import Settings
 import pytest
 
 
-ROUTE = Settings.URL + Settings.DELETE
+def request(url, params):
+    ROUTE = Settings.URL + Settings.DELETE
+
+    with Client(base_url=ROUTE) as c:
+        response = c.delete(url=url, params=params)
+    return response
 
 
 @pytest.mark.parametrize(
@@ -30,7 +34,56 @@ ROUTE = Settings.URL + Settings.DELETE
         ("/cabinet", {"document_id": "HUQD0azry2g7e7WpHIrV"}),
     ],
 )
-def test_delete(prefix, params):
-    with Client(base_url=ROUTE) as c:
-        response = c.delete(prefix, params=params)
-    assert response.status_code == codes.NO_CONTENT
+def test_successful_delete(prefix, params):
+    assert request(prefix, params).status_code == codes.NO_CONTENT
+
+
+@pytest.mark.parametrize(
+    "prefix, params",
+    [
+        ("/user", {"document_id": ""}),
+        ("/product", {"document_id": ""}),
+        (
+            "/order",
+            {"document_id": ""},
+        ),
+        (
+            "/product_category",
+            {"document_id": ""},
+        ),
+        (
+            "/office",
+            {"document_id": ""},
+        ),
+        ("/cabinet", {"document_id": ""}),
+    ],
+)
+def test_badrequest1_delete(prefix, params):
+    assert request(prefix, params).status_code == codes.BAD_REQUEST
+
+
+@pytest.mark.parametrize(
+    "prefix, params",
+    [
+        ("/use", {"document_id": "267U3f0pKDfnKWx4N0tq"}),
+        (
+            "/prouct",
+            {"document_id": "pa9xFlcIXcyDvCA5onWE"},
+        ),
+        (
+            "/orer",
+            {"document_id": "BI6xTsN60lxZUcrW5FA9"},
+        ),
+        (
+            "/виаиваи",
+            {"document_id": "U0Kg4CtDyNRTnLpfa08V"},
+        ),
+        (
+            "/342",
+            {"document_id": "U0Kg4CtDyNRTnLpfa08V"},
+        ),
+        ("/34ацуывПШГЛ", {"document_id": "HUQD0azry2g7e7WpHIrV"}),
+    ],
+)
+def test_notfound_delete(prefix, params):
+    assert request(prefix, params).status_code == codes.UNPROCESSABLE_ENTITY
