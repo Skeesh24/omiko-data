c153a1980ac7af47e11924abc68a40187a4f37ea
skeesh24 <d.pacooloff@gmail.com>
2023-08-10 23:49:24 +1000

    len check added

--- app/classes/functions.py
+++ app/classes/functions.py
@@ -7,3 +7,10 @@ def try_get_repository(uow, tablename):
         return uow.__getattribute__(tablename)
     except:
         raise HTTPException(codes.UNPROCESSABLE_ENTITY, detail=tablename)
+
+
+def is_list(elements):
+    try:
+        return len(elements) >= 0
+    except:
+        return False
