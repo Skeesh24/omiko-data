a8cb01b6b62e311cdb746f1332b166e2f8fd9816
skeesh24 <d.pacooloff@gmail.com>
2023-10-16 21:36:21 +0500

    root router's package ix

--- app/routers/root_router.py
+++ app/routers/root_router.py
@@ -1,12 +1,10 @@
+from database.firestore.firebase import initialize
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .select_router import select_router
-from .insert_router import insert_router
-from .update_router import put_router
-from .delete_router import delete_router
-
-from app.database.firebase import initialize
-
+from routers.delete_router import delete_router
+from routers.insert_router import insert_router
+from routers.select_router import select_router
+from routers.update_router import put_router
 
 app = FastAPI()
 app.add_middleware(
@@ -20,10 +18,6 @@ app.include_router(select_router)
 app.include_router(insert_router)
 app.include_router(put_router)
 app.include_router(delete_router)
-
-
-# db init
-initialize()
 
 
 @app.get("/")
